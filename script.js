f2bcb4981ab15495374e77162aa7f40919176d8b
Rauf221 <Rauf280604@gmail.com>
2024-05-23 18:36:44 +0400

    added new test

---
+++ script.js
@@ -0,0 +1,45 @@
+ const link = document.createElement("a")
+ link.innerHTML = "Click me"
+ link.className = "btn btn-warning"
+ link.href = "https://www.google.com"
+ link.target = "_blank"
+
+ const cardBody = document.querySelector("body")
+ cardBody.appendChild(link)
+
+
+
+const awidam = document.getElementsByTagName(h1);
+console.log(awidam);
+
+const awdmadm = document.getElementsByTagName(h2);
+console.log(awdmadm);
+
+const fmsdfgmv =document.getElementsByTagName(h3);
+console.log(fmsdfgmv);
+
+
+
+
+const firstTitle = document.querySelector('');
+console.log(firstTitle);
+
+const firstTitle1 =document.querySelector();
+console.log(firstTitle1);
+
+const navvv = document.getElementById("wodaoda").firstElementChild;
+console.log(navvv);
+
+
+
+const allTitles = document.querySelectorAll('')
+console.log(allTitles);
+
+
+console.log(allTitles.length);
+for (let i = 0; i < allTitles.length; i++) {
+    console.log(allTitles[i])
+}
+
+const nammeee =document.getElementsByClassName('isenfoi');
+console.log(nammeee);
